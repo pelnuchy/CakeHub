2df0f47ab11ff5892fff126a220e0a2aea609bfb
Shiphuu <psphu2103@gmail.com>
2024-06-04 22:53:40 +0700

    fix[all file code]: fix tabs size = 4 spaces

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold ">
-      Hello world! phuc ne
-    </h1>
-  )
+	return (
+    	<h1 className="text-3xl font-bold text-[#F60003] hover:text-blue-400">
+        	Hello world! phuc ne
+    	</h1>
+    )
 }
