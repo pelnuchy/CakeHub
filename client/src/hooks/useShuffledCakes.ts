1dbe5c583e0d55e993640a9e7d46efe4f4c49807
pearhp <leehuynhphuc@gmail.com>
2024-06-27 15:26:30 +0700

    fix: hook NumCake

--- client/src/hooks/useShuffledCakes.ts
+++ client/src/hooks/useShuffledCakes.ts
@@ -2,13 +2,13 @@ import { useState, useEffect } from 'react';
 import { cakeData, Cake } from '../utils/cakeData';
 import { shuffleArray } from '../utils/shuffleArray';
 
-export const useShuffledCakes = (numCake : number): Cake[] => {
+export const useShuffledCakes = (numCake: number): Cake[] => {
   const [randomCakes, setRandomCakes] = useState<Cake[]>([]);
 
   useEffect(() => {
     const shuffledCakes = shuffleArray(cakeData).slice(0, numCake);
     setRandomCakes(shuffledCakes);
-  }, []);
+  }, [numCake]);
 
   return randomCakes;
 };
