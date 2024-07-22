8b93721513a92ba5fb02eef3b0b923754a1340e8
duckien2012 <hoanganhtra181@gmail.com>
2024-07-22 16:10:39 +0700

    fix: delete type button

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -7,10 +7,9 @@ type ButtonProps = {
   children: React.ReactNode;
 };
 
-const Button: React.FC<ButtonProps> = ({ type, className, onClick, children }) => {
+const Button: React.FC<ButtonProps> = ({ className, onClick, children }) => {
   return (
     <button
-      type="button"
       className={`rounded bg-gradient-to-t from-primary-500 to-yellow-500 px-6 py-3 font-semibold text-black hover:scale-105 hover:text-white ${className} transition duration-100 ease-in-out`}
       onClick={onClick}
     >
