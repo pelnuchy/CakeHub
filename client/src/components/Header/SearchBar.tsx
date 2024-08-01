19a60168e067bb68b32a209133ca915313675a62
pearhp <leehuynhphuc@gmail.com>
2024-08-01 22:48:01 +0700

    feat: keyword search at searchbar

--- client/src/components/Header/SearchBar.tsx
+++ client/src/components/Header/SearchBar.tsx
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
 
-const SearchBar: React.FC = () => (
-  <div className="flex flex-auto items-center justify-center">
-    <div className="flex h-10 w-full items-center rounded-xl bg-white px-4 py-1">
-      <input
-        type="text"
-        placeholder="Tìm kiếm bánh"
-        className="w-full rounded-l-full bg-transparent px-2 py-1 text-black outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50"
-      />
-      <FaSearch className="mr-2 h-8 w-8 transform bg-white p-1 text-black transition-transform duration-300 ease-in-out hover:scale-110" />
+const SearchBar: React.FC = () => {
+  const [keyword, setKeyword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSearch = () => {
+    if (keyword.trim()) {
+      navigate(`/search/${keyword}`);
+    }
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
+  return (
+    <div className="flex flex-auto items-center justify-center">
+      <div className="flex h-10 w-full items-center rounded-xl bg-white px-4 py-1">
+        <input
+          type="text"
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+          onKeyDown={handleKeyDown}
+          placeholder="Tìm kiếm bánh"
+          className="w-full rounded-l-full bg-transparent px-2 py-1 text-black outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50"
+        />
+        <FaSearch
+          className="mr-2 h-8 w-8 transform bg-white p-1 text-black transition-transform duration-300 ease-in-out hover:scale-110"
+          onClick={handleSearch}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default SearchBar;
---
+++ client/src/layouts/SearchResult.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import CakeCard from '../components/Cake/CakeCard';
+
+const SearchResults: React.FC = () => {
+  const { keyword } = useParams<{ keyword: string }>();
+  const [searchResults, setSearchResults] = useState<object[]>([]);
+
+  useEffect(() => {
+    const fetchSearchResults = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8000/search-cakes?keyword=${keyword}`);
+        setSearchResults(response.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchSearchResults();
+  }, [keyword]);
+
+  return (
+    <div className="bg-white py-16">
+      <div className="container ml-9">
+        <div className="mb-4 flex items-center">
+          <div className="mr-2 h-8 w-4 bg-bgr-gradient"></div>
+          <h2 className="text-3xl font-bold">Kết quả tìm kiếm cho "{keyword}"</h2>
+        </div>
+
+        <div className="grid grid-cols-1 gap-16 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {searchResults.map((cake, index) => (
+            <CakeCard key={index} cake={cake} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchResults;
