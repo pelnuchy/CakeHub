e8a5e0b2dc8891b4076b42ee4a40379adb78ed0c
pearhp <leehuynhphuc@gmail.com>
2024-08-03 11:08:50 +0700

    fix: sortcontrol for trending cake

--- client/src/layouts/CakeList/Trending.tsx
+++ client/src/layouts/CakeList/Trending.tsx
@@ -1,8 +1,35 @@
+import React, { useState, useEffect } from 'react';
 import CakeCard from '../../components/Cake/CakeCard';
 import { useShuffledCakes } from '../../hooks/useShuffledCakes';
+import SortControl from '../Occasion/SortControl';
 
 const Trending = () => {
+  const [sortOption, setSortOption] = useState<string>('');
+  const [sortedCakes, setSortedCakes] = useState<object[]>([]);
   const randomCakes = useShuffledCakes(8);
+
+  useEffect(() => {
+    setSortedCakes(sortCakes(randomCakes, sortOption));
+  }, [randomCakes, sortOption]);
+
+  const handleSortChange = (option: string) => {
+    setSortOption(option);
+  };
+
+  const sortCakes = (cakes: any[], option: string) => {
+    switch (option) {
+      case 'nameAsc':
+        return cakes.slice().sort((a, b) => a.cakeName.localeCompare(b.cakeName));
+      case 'nameDesc':
+        return cakes.slice().sort((a, b) => b.cakeName.localeCompare(a.cakeName));
+      case 'priceAsc':
+        return cakes.slice().sort((a, b) => a.price - b.price);
+      case 'priceDesc':
+        return cakes.slice().sort((a, b) => b.price - a.price);
+      default:
+        return cakes;
+    }
+  };
 
   return (
     <div className="bg-white px-4 py-16">
@@ -14,9 +41,9 @@ const Trending = () => {
           </div>
           <span className="cursor-pointer text-blue-600 hover:underline">Xem tất cả</span>
         </div>
-
+        <SortControl sortOption={sortOption} onSortChange={handleSortChange} />
         <div className="grid gap-16 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {randomCakes.map((cake, index) => (
+          {sortedCakes.map((cake, index) => (
             <CakeCard key={index} cake={cake} />
           ))}
         </div>
