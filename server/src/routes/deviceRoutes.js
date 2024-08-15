572491f90e07a407153310cf91bed6e7c4b5d276
duckien2012 <hoanganhtra181@gmail.com>
2024-08-15 17:27:59 +0700

    fix[deviceRoutes]: delete router add device

--- server/src/routes/deviceRoutes.js
+++ server/src/routes/deviceRoutes.js
@@ -3,7 +3,6 @@ import deviceController from '../controllers/deviceController.js';
 const router = express.Router();
 
 router.get('/get-all-devices', deviceController.getAllDevices);
-router.put('/add-device', deviceController.addDevice);
 router.put('/update-quantity-device/:device_id/device', deviceController.updateQuantityDevice);
 router.put('/delete-device/:id', deviceController.deleteDevice);
 
