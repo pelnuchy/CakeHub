49658e2934db4dffa8e377fdecf620de61b7de34
pearhp <leehuynhphuc@gmail.com>
2024-08-21 22:33:46 +0700

    fix2

--- server/server.js
+++ server/server.js
@@ -1,33 +1,35 @@
-import express, { Router } from 'express';
-import mongoose from 'mongoose';
-import "dotenv/config"
-import userRoutes from './src/routes/userRoutes.js';
-import cakeRoutes from './src/routes/cakeRoutes.js';
-import cartRoutes from './src/routes/cartRoutes.js';
-import orderRoutes from './src/routes/orderRoutes.js';
-import ingredientRoutes from './src/routes/ingredientRoutes.js';
-import deviceRoutes from './src/routes/deviceRoutes.js';
-import paymentRoutes from './src/routes/paymentRoutes.js';
-import bodyParser from 'body-parser';
-import session from 'express-session';
-import cors from 'cors';
+import express, { Router } from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+import userRoutes from "./src/routes/userRoutes.js";
+import cakeRoutes from "./src/routes/cakeRoutes.js";
+import cartRoutes from "./src/routes/cartRoutes.js";
+import orderRoutes from "./src/routes/orderRoutes.js";
+import ingredientRoutes from "./src/routes/ingredientRoutes.js";
+import deviceRoutes from "./src/routes/deviceRoutes.js";
+import paymentRoutes from "./src/routes/paymentRoutes.js";
+import bodyParser from "body-parser";
+import session from "express-session";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
-app.use(session({
-    name: 'sid',
+app.use(
+  session({
+    name: "sid",
     resave: false,
     saveUninitialized: false,
-    secret: 'kobietgihet',
+    secret: "kobietgihet",
     cookie: {
-        maxAge: 1000 * 60 * 60 * 2,
-        sameSite: true,
-        secure: false,
-    }
-}));
+      maxAge: 1000 * 60 * 60 * 2,
+      sameSite: true,
+      secure: false,
+    },
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -35,18 +37,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(
+  "/",
+  cakeRoutes,
+  userRoutes,
+  cartRoutes,
+  orderRoutes,
+  ingredientRoutes,
+  deviceRoutes,
+  paymentRoutes
+);
 
-app.use('/',cakeRoutes, userRoutes, cartRoutes, orderRoutes, ingredientRoutes, deviceRoutes, paymentRoutes);
-
-
-mongoose.connect(process.env.DATABASE_URI)
-.then(async () => { // Sử dụng async để có thể sử dụng await bên trong
-    console.log('Connected to MongoDB');
-    console.log('Current Database:', mongoose.connection.db.databaseName);
+mongoose
+  .connect(process.env.DATABASE_URI)
+  .then(async () => {
+    // Sử dụng async để có thể sử dụng await bên trong
+    console.log("Connected to MongoDB");
+    console.log("Current Database:", mongoose.connection.db.databaseName);
     app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
+      console.log(`Server is running on port ${PORT}`);
     });
-})
-.catch((error) => {
-    console.log(error)
-});
+  })
+  .catch((error) => {
+    console.log(error);
+  });
