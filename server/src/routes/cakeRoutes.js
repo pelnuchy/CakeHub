7b40ccbb7ca4f73c2a2792a257d90d861f30be0a
pearhp <leehuynhphuc@gmail.com>
2024-08-31 16:04:14 +0700

    fix: multer

--- server/src/routes/cakeRoutes.js
+++ server/src/routes/cakeRoutes.js
@@ -1,19 +1,24 @@
-import express from 'express';
-import cakeController from '../controllers/cakeController.js';
-import multer from 'multer';
+import express from "express";
+import cakeController from "../controllers/cakeController.js";
+import multer from "multer";
 
-const upload = multer({ dest: "uploads/" });
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
 const router = express.Router();
 
 //client
-router.get('/get-all-cakes', cakeController.getAllCakes);
-router.get('/get-all-cakes-occasion/:occasion', cakeController.getAllCakesOccasion);
-router.get('/get-details-cake/:id', cakeController.getDetailCake);
-router.get('/get-cake-related/:id', cakeController.getRelatedCakes);
+router.get("/get-all-cakes", cakeController.getAllCakes);
+router.get(
+  "/get-all-cakes-occasion/:occasion",
+  cakeController.getAllCakesOccasion
+);
+router.get("/get-details-cake/:id", cakeController.getDetailCake);
+router.get("/get-cake-related/:id", cakeController.getRelatedCakes);
 router.get("/search-cakes", cakeController.searchCakesByKeyword);
 
 //baker
-router.get('/get-all-cakes-baker', cakeController.getAllCakesManageCake);
+router.get("/get-all-cakes-baker", cakeController.getAllCakesManageCake);
 router.post("/add-cake", upload.single("image"), cakeController.addCake);
 router.delete("/delete-cake/:id", cakeController.deleteCake);
-export default router; 
+export default router;
