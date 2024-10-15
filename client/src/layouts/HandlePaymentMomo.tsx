062a4390290946579bec3189a1ae2de0c246a54a
pelnuchy <leehuynhphuc@gmail.com>
2024-10-16 00:13:02 +0700

    update(HandlePage): spinner UI

--- client/src/layouts/HandlePaymentMomo.tsx
+++ client/src/layouts/HandlePaymentMomo.tsx
@@ -3,7 +3,6 @@ import axios from 'axios';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { useCart } from '../contexts/CartContext';
 
-
 const HandlePayment = () => {
   const navigate = useNavigate();
 
@@ -12,7 +11,6 @@ const HandlePayment = () => {
   if (!userInfo || userInfo.role !== 'customer') {
     navigate('/login');
   }
-
 
   const location = useLocation();
 
@@ -37,6 +35,8 @@ const HandlePayment = () => {
           console.log('Transaction successful');
           await axios.post(`${process.env.REACT_APP_API_URL}/create-order`, { orderDetail });
           await axios.put(`${process.env.REACT_APP_API_URL}/remove-all-cakes-from-cart/${userInfo.userID}/cart`);
+          //delay 5 seconds
+          await new Promise((resolve) => setTimeout(resolve, 5000));
           navigate('/purchase');
         } else {
           navigate('/checkout');
@@ -54,11 +54,22 @@ const HandlePayment = () => {
     } else {
       console.log('No Order ID found in URL');
     }
-
-  }, []);
+  }, [location.search, navigate, userInfo.userID]);
   return (
-    <div>
-      <p>Đang xử lý thanh toán...</p>
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <div className="flex items-center space-x-2">
+        <svg
+          className="h-5 w-5 animate-spin text-primary-500"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+          <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"></path>
+        </svg>
+
+        <p className="text-lg font-medium text-gray-800">Đang xử lý thanh toán...</p>
+      </div>
     </div>
   );
 };
